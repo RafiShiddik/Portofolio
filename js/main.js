e0eb33ad57387bdf7016c11911aef274a46e62bf
RafiShiddik <rafishiddik@gmail.com>
2025-08-10 22:29:52 +0700

    Upload website

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('.menu');
+const hamburgermenu = document.querySelector('.hamburger-menu');
+const iconBars = document.querySelector('.icon-bars');
+const iconClose = document.querySelector('.icon-close');
+hamburgermenu.addEventListener('click',displayMenu)
+menu.addEventListener('click',displayMenu)
+
+function displayMenu()(
+    if(menu.classList.contains('tampil'))(
+        menu.classList.remove('tampil');
+        iconBars.style.display='inline';
+        iconClose.style.display='none';
+    )else(
+        menu.classList.add('tampil');
+        iconBars.style.display='none';
+        iconClose.style.display='inline';
+    )
+)
